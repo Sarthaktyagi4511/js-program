14055222befdf08253587b14207cc123b381dd1d
sarthak tyagi <91381234+Sarthaktyagi4511@users.noreply.github.com>
2025-07-17 16:20:22 +0000

    datatype

---
+++ javascript/1_datatype.js
@@ -0,0 +1,32 @@
+// two types of datatype primitve and refernce or non primiitve datatype
+
+//primitive:- string, number,bigInt,boolean,null,undefined.
+const score = 100
+const scorevalue =100.3
+
+const isLoggedin = false
+const outsidetemp = null
+
+
+//non primitive:- array,function,object.
+
+
+
+//array
+const heros=['ironman','superman','batman']
+
+
+
+//abject
+let myobj={
+
+    name:'sarthak',
+    age:22,
+}
+
+
+
+//object
+const myfunction= function(){
+    console.log("helllloo sarthak")
+}
