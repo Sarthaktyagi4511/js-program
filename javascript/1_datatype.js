1d7e20cdb0cc6f3ecb6d50d0df6b469d03e02dfc
sarthak tyagi <91381234+Sarthaktyagi4511@users.noreply.github.com>
2025-07-29 08:21:55 +0000

    json

--- javascript/1_datatype.js
+++ javascript/1_datatype.js
@@ -1,32 +1,84 @@
 // two types of datatype primitve and refernce or non primiitve datatype
 
 //primitive:- string, number,bigInt,boolean,null,undefined.
-const score = 100
-const scorevalue =100.3
+// const score = 100
+// const scorevalue =100.3
 
-const isLoggedin = false
-const outsidetemp = null
-
-
-//non primitive:- array,function,object.
+// const isLoggedin = false
+// const outsidetemp = null
 
 
-
-//array
-const heros=['ironman','superman','batman']
+// //non primitive:- array,function,object.
 
 
 
-//abject
-let myobj={
+// //array
+// const heros=['ironman','superman','batman']
 
-    name:'sarthak',
+
+
+// objects
+
+// singleton Object
+const JsUser ={
+    name:'sarthak tyagi',
     age:22,
+    email:'sarthak@gmail.com',
+    location:'noida'
+
+
+}
+
+// console.log(JsUser.name);
+// console.log(JsUser["name"]);
+
+// non-singleton object
+
+// const tinderUser = new Object()
+const tinderUser={
+    userID:"124mvksmdv",
+    UserName:"sarthak",
+    UserLoaction:"Noida"
+}
+
+// tinderUser.userID = "34343cdnclsnds"
+
+// console.log(tinderUser);
+
+
+const obj1={
+    rollNo:23
 }
 
 
+const obj2=obj1
 
-//object
-const myfunction= function(){
-    console.log("helllloo sarthak")
+obj2.rollNo=45
+// console.log(obj2)
+// console.log(obj1)
+
+
+const regularUser={
+    email:"fnsdfjhjdds",
+    username:{
+        fullname:{
+            firstname:"sarthak",
+            lastname:"tyagi"
+        }
+    }
 }
+// console.log(regularUser.username.fullname);
+
+// console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(tinderUser.hasOwnproperty('isloggedin'))
+
+
+const course ={
+    coursename: "javascript",
+    courseprice:"999",
+    coursemode:"online"
+}
+const {coursename:helllo} = course
+
+console.log(helllo);
