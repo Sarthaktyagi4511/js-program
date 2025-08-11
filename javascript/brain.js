8bcb72087b8c8066e247e3d059f0e6b4d4a3c5af
sarthak tyagi <91381234+Sarthaktyagi4511@users.noreply.github.com>
2025-08-11 16:20:46 +0000

    brain

--- javascript/brain.js
+++ javascript/brain.js
@@ -13,18 +13,3 @@ buttons.forEach(function(button){
     
 });
 
-
-
-
-
-
-
-
-// const buttons = document.querySelectorAll(".button");
-// const body = document.body;
-
-// buttons.forEach(button => {
-//     button.addEventListener('click', function(e) {
-//         body.style.backgroundColor = e.target.id; 
-//     });
-// });
